93c17004d724ed73e26f1fc379ad041ffb5de45b
Sagar Saini <c0827838@mylambton.ca>
2022-02-12 00:42:13 -0500

    Date Feature Added

--- app/src/main/java/com/example/task_theavengers_android/CreateTaskActivity.java
+++ app/src/main/java/com/example/task_theavengers_android/CreateTaskActivity.java
@@ -6,6 +6,8 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,6 +18,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
@@ -26,7 +29,10 @@ import android.widget.ViewSwitcher;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class CreateTaskActivity extends AppCompatActivity {
     EditText title,categorySelected,desc;
@@ -42,6 +48,8 @@ public class CreateTaskActivity extends AppCompatActivity {
     private static final int PICK_IMAGES_CODE=0;
     int position = 0;
 
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +61,9 @@ public class CreateTaskActivity extends AppCompatActivity {
         imageURI = new ArrayList<>();
         img = findViewById(R.id.imgdemo);
         isRecording=false;
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
 
 
         // Requesting Mic Permissions
@@ -169,6 +180,88 @@ public class CreateTaskActivity extends AppCompatActivity {
     });
     }
 
+
+//--------------------------Date Picker Functions--------------------------------
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+       // datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+       // datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+
+        return "JAN";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+
+
 //-------------------Functions for Picking Images---------------------------
 
     private void pickImageIntent(){
@@ -233,6 +326,7 @@ public class CreateTaskActivity extends AppCompatActivity {
         File file = new File(musicDirectory,"testname"+".mp3");
         return file.getPath();
     }
+
 
 
 }
